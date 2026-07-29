12a4bd290eab6b48823b073d962943a84516b58b
dankalawa860-commits <dankalawa860@gmail.com>
2026-07-29 23:11:27 +0100

    Add signup function for user registration

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 let balance = 0;
 
+function signup() {
+  let name = document.getElementById("name").value;
+
+  if (name == "") {
+    alert("Please enter your name");
+    return;
+  }
+
+  alert("Welcome " + name + "!");
+}
+
 function completeTask(amount) {
   balance += amount;
   document.getElementById("balance").textContent = balance;
