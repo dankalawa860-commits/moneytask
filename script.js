96ac4976ad2f7abe0c1e95d155b45b418b457546
dankalawa860-commits <dankalawa860@gmail.com>
2026-07-29 22:51:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let balance = 0;
 const button = document.querySelector("button");
 const balanceText = document.querySelectorAll("h1")[1];
 
-button.addEventListener("click", () => {
-  balance += 1;
-  balanceText.innerText = "$" + balance;
-});
+button.onclick = function () {
+  balance++;
+  balanceText.innerHTML = "$" + balance;
+  alert("Task Completed! +$1");
+};
