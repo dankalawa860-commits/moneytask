ad357b6241fd0d52592e603b70c3ff8e18d0036c
dankalawa860-commits <dankalawa860@gmail.com>
2026-07-29 22:54:03 +0100

    Refactor balance handling with localStorage
    Updated balance initialization to use localStorage and added functionality to save balance on button click.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let balance = 0;
+let balance = localStorage.getItem("balance") || 0;
 
 const button = document.querySelector("button");
 const balanceText = document.querySelectorAll("h1")[1];
 
+balanceText.innerHTML = "$" + balance;
+
 button.onclick = function () {
   balance++;
   balanceText.innerHTML = "$" + balance;
+  localStorage.setItem("balance", balance);
   alert("Task Completed! +$1");
 };
